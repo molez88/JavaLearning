b79c6371c71c1c534f5d36ec25af6f2a44611e99
Ramya Tammisetti <ramya.tammisetti@gmail.com>
2016-03-21 10:27:04 -0400

    Changes the lookup value in SortMethods from 20 to 40.

--- 2016-03-11 Sorting/src/SortMethods.java
+++ 2016-03-11 Sorting/src/SortMethods.java
@@ -22,7 +22,7 @@ public class SortMethods {
 		
 		int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 		
-		int value = 20;
+		int value = 40;
 		int dex = BinarySearch.search(nums, value);
 		
 		
